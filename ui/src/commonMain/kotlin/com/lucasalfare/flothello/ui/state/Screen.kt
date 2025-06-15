7b0dc1470ba0e62080ab90756e50b43d0b90f193
LucasAlfare <god-lucas@hotmail.com>
2025-06-15 00:32:53 -0300

    Migrate Screen objects to data objects
    The `Home` and `Game` objects within the `Screen` sealed class have been changed to data objects. This leverages the benefits of data classes, such as automatically generated `equals()`, `hashCode()`, and `toString()` methods, which can be useful for state management and debugging.

--- ui/src/commonMain/kotlin/com/lucasalfare/flothello/ui/state/Screen.kt
+++ ui/src/commonMain/kotlin/com/lucasalfare/flothello/ui/state/Screen.kt
@@ -1,6 +1,6 @@
 package com.lucasalfare.flothello.ui.state
 
 sealed class Screen {
-  object Home : Screen()
-  object Game : Screen()
+  data object Home : Screen()
+  data object Game : Screen()
 }
